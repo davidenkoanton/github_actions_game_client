ced51316bbd5d3f55ce2ddab4d8340d6a55ec22e
Anton Davidenko <a.davidenko@pls.life>
2022-04-06 13:41:54 +0300

    check commit message

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  console.log(`payload.commits: ${payload.commits}`);
+  console.log(`payload.commits[0]: ${payload.commits[0]}`);
+  console.log(`payload.commits[0].message: ${payload.commits[0].message}`);
   console.log('---=== START FROM DIRRECT REPO ===---');
 } catch (error) {
   core.setFailed(error.message);
